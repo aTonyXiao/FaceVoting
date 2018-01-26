3c11432ec61df05a3c6e7cd7f6c2c9a367a76ddf
aTonyXiao <tony.xiao95@icloud.com>
2018-01-25 19:44:10 -0800

    finished

--- app/components/App.js
+++ app/components/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Footer from './Footer';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar history={this.props.history} />
-                {this.props.children}
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Navbar history={this.props.history} />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- app/stores/FooterStore.js
+++ app/stores/FooterStore.js
@@ -2,19 +2,18 @@ import alt from '../alt';
 import FooterActions from '../actions/FooterActions';
 
 class FooterStore {
-    constructor() {
-        this.bindActions(FooterActions);
-        this.characters = [];
-    }
+  constructor() {
+    this.bindActions(FooterActions);
+    this.characters = [];
+  }
 
-    onGetTopCharactersSuccess(data) {
-        this.characters = data.slice(0, 5);
-    }
+  onGetTopCharactersSuccess(data) {
+    this.characters = data.slice(0, 5);
+  }
 
-    onGetTopCharactersFail(jqXhr) {
-        // Handle multiple response formats, fallback to HTTP status code number.
-        toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
-    }
+  onGetTopCharactersFail(jqXhr) {
+    toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
+  }
 }
 
 export default alt.createStore(FooterStore);
